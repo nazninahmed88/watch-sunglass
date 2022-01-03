14b63bb065f7bf0e052b0a63f02a9d53bf5ad756
nazninahmed88 <rinkienaznin88@gmail.com>
2022-01-03 23:22:50 +0600

    sceond commit

---
+++ src/Pages/Home/ReViews/ReViews.js
@@ -0,0 +1,32 @@
+import { Container, Grid, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import ShowReviews from '../ShowReviews/ShowReviews';
+
+const ReViews = () => {
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+        fetch('https://pacific-harbor-22675.herokuapp.com/reviews')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+    return (
+        <div>
+            <Container sx={{ my: 5 }}>
+                <Typography  sx={{my:3,color:'info.main'}} variant="h5">
+                    Customer Reviews 
+                </Typography>
+                <Grid container spacing={2}>
+                {
+                    reviews.map(review => <ShowReviews
+                        review={review}
+                        key={review._id} >
+                    </ShowReviews>)
+
+                }
+                </Grid>
+            </Container>
+        </div>
+    );
+};
+
+export default ReViews;
